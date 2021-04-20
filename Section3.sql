b4556152f86dee147c5bfbbc6d2df704ffd75f1f
Nirajan Budhathoki <statnirajan@gmail.com>
2021-04-20 19:50:37 -0400

    Add files via upload

---
+++ Section3.sql
@@ -0,0 +1,104 @@
+
+/* SECTION 3: GROUP BY statements */
+
+/* Aggregation Functions */
+
+SELECT MIN (replacement_cost) FROM film;
+SELECT AVG (replacement_cost) FROM film;
+SELECT ROUND (AVG (replacement_cost),3) FROM film;
+SELECT MIN (replacement_cost), MAX (replacement_cost) FROM film;
+SELECT COUNT (*) FROM film;
+SELECT SUM (replacement_cost) FROM film;
+
+/* GROUP BY */ 
+/* The GROUP BY clause must appear right after a FROM or WHERE statement */
+
+SELECT * FROM payment;
+SELECT SUM (amount) FROM payment
+GROUP BY customer_id;
+SELECT customer_id, SUM (amount) FROM payment
+GROUP BY customer_id;
+SELECT customer_id, SUM (amount) FROM payment
+GROUP BY customer_id
+ORDER BY SUM (amount);
+SELECT customer_id, staff_id, SUM (amount) FROM payment
+GROUP BY staff_id, customer_id
+ORDER BY customer_id;
+SELECT customer_id, staff_id, SUM (amount) FROM payment
+GROUP BY staff_id, customer_id
+ORDER BY customer_id, staff_id;
+SELECT DATE (payment_date), SUM (amount) FROM payment
+GROUP BY DATE (payment_date);
+SELECT DATE (payment_date), SUM (amount) FROM payment
+GROUP BY DATE (payment_date)
+ORDER BY SUM (amount) DESC;
+
+/* Challenge: GROUP BY */
+/* 1. We have two staff id 1 and 2. We want to give a bonus to the staff member that handled the most payments. How many payments did each staff member handle and who gets the bonus */
+
+SELECT * FROM payment;
+SELECT staff_id, COUNT (*) FROM payment
+GROUP BY staff_id;
+
+/* 2. What is the average replacement cost per MPAA rating */
+SELECT * FROM film;
+SELECT rating, AVG (replacement_cost) FROM film
+GROUP BY rating;
+
+/* 3. What are the customer ids of the top 5 customers by total spend */
+SELECT * FROM payment;
+SELECT customer_id, SUM (amount) FROM payment
+GROUP BY customer_id
+ORDER BY SUM (amount) DESC
+LIMIT 5;
+
+/* HAVING */
+
+SELECT * FROM payment
+SELECT customer_id, SUM (amount) FROM payment
+GROUP BY customer_id
+HAVING SUM (amount) > 100;
+
+SELECT * FROM customer;
+SELECT store_id, COUNT (*) FROM customer
+GROUP BY store_id
+HAVING COUNT (*) > 300
+
+
+/* Challenge: HAVING */
+/* 1. We will assign platinum status to customers that have had 40 or more transaction payments. What customers_id are eligible */
+
+SELECT * FROM payment
+SELECT customer_id, COUNT (*) FROM payment
+GROUP BY customer_id
+HAVING COUNT (*) >= 40
+
+/* 2. What are the customer ids of customers who have spent more than $100 in payment transactions with staff _id member 2 */
+
+SELECT customer_id, SUM (amount) FROM payment
+WHERE staff_id = 2
+GROUP BY customer_id
+HAVING SUM (amount) > 100;
+
+/* ASSESSMENT TEST 1 */
+/* 1. Return the customer IDs of customers who have spent at least $110 with the staff member who has an ID of 2. */
+
+/* 2. How many films begin with the letter J? */
+
+/* 3. What customer has the highest customer ID number whose name starts with an 'E' and has an address ID lower than 500? */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
