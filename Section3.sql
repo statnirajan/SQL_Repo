844ab23e68a6d43c0fb2b20d1f75e31e5c055f74
Nirajan Budhathoki <statnirajan@gmail.com>
2021-10-30 14:28:06 -0400

    Update Section3.sql

--- Section3.sql
+++ Section3.sql
@@ -80,13 +80,6 @@ WHERE staff_id = 2
 GROUP BY customer_id
 HAVING SUM (amount) > 100;
 
-/* ASSESSMENT TEST 1 */
-/* 1. Return the customer IDs of customers who have spent at least $110 with the staff member who has an ID of 2. */
-
-/* 2. How many films begin with the letter J? */
-
-/* 3. What customer has the highest customer ID number whose name starts with an 'E' and has an address ID lower than 500? */
-
 
 
 
