6e5ff5c49a12a37007f30da38a50430d7fac03e6
Nirajan Budhathoki <statnirajan@gmail.com>
2023-11-26 14:14:14 -0500

    Update AssessmentTest1.sql

--- AssessmentTest1.sql
+++ AssessmentTest1.sql
@@ -1,1 +1,9 @@
 
+/* 1. Return the customer IDs of customers who have spent at least $ 110 with the staff member who has an ID of 2 */
+SELECT customer_id, SUM(amount)  
+FROM payment
+WHERE staff_id = 2
+GROUP BY customer_id
+HAVING SUM(amount) >110;
+
+/* 2. How many films begin with the letter J ? */
