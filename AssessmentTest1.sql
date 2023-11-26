6c551915e78819834c8acb1ec8004d131cb4e68b
Nirajan Budhathoki <statnirajan@gmail.com>
2023-11-26 14:22:01 -0500

    Update AssessmentTest1.sql

--- AssessmentTest1.sql
+++ AssessmentTest1.sql
@@ -7,3 +7,13 @@ GROUP BY customer_id
 HAVING SUM(amount) >110;
 
 /* 2. How many films begin with the letter J ? */
+SELECT COUNT(*) 
+FROM film
+WHERE title  LIKE 'J%';
+
+/* 3. What customer has the highest customer ID number whose name starts with an 'E' and has an address ID lower than 500 ? */
+
+SELECT first_name, last_name  FROM customer
+WHERE first_name LIKE 'E%' AND address_id < 500
+ORDER BY customer_id DESC
+LIMIT 1;
